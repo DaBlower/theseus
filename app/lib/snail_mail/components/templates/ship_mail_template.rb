16e8f6c8027c4e545edd75859e5de87483d0bbb0
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-08-31 23:07:18 +1000

    fix up values

--- app/lib/snail_mail/components/templates/ship_mail_template.rb
+++ app/lib/snail_mail/components/templates/ship_mail_template.rb
@@ -5,34 +5,30 @@ module SnailMail
     module Templates
       class ShipMailTemplate < TemplateBase
         def self.template_name
-            "Ship Mail"
+          "Ship Mail"
         end
 
-        def self.show_on_single
-            true
+        def self.show_on_single?
+          true
         end
 
         def view_template
-            render_return_address(5, bounds.top - 5, 190, 90, size: 8, font: "f25")
+          image(
+            image_path("ship_mail.png"),
+            at: [-2.5, 288],
+            width: 443,
+          )
 
-            render_destination_address(
-                120,
-                115,
-                270,
-                81,
-                size: 18, valign: :center, align: :left
-            )
-
-            image(
-                image_path("ship_mail.png")
-                at: [0, 0],
-            )
-
-            render_imb(245, 20, 170)
-            render_letter_id(3, 15, 8, rotate: 90)
-            render_qr_code(2, 52, 50)
-            render_postage
-
+          render_return_address(5, bounds.top - 5, 190, 90, size: 8, font: "f25")
+          render_destination_address(115, bounds.top - 85, 210, 100, size: 18, valign: :center, align: :left)
+          
+          render_imb(150, 0, 270)
+          render_qr_code(162.5, 25, 45)
+          render_letter_id(225, 25, 8)
+          render_postage
+          end
+        end
+      end
     end
   end
 end
